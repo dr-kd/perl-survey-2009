d0c15cd6a577e06f61b29b0d2d1f1486e22c33f5
Kieren Diment <kd@fenchurch.local>
2009-09-12 10:33:29 +1000

    redirect now works

--- Survey-Perl/lib/Survey/Perl/Controller/ReCaptcha.pm
+++ Survey-Perl/lib/Survey/Perl/Controller/ReCaptcha.pm
@@ -4,15 +4,21 @@ BEGIN { extends 'Catalyst::Controller::reCAPTCHA' }
 
 sub test_user :Private {
     my ($self, $c) = @_;
-    return 1 if $c->session->{human} ;
-    $c->forward('captcha_check');
-    if ($c->stash->{recaptcha_error}) {
-        $c->stash( template => 'auth.tt');
-        $c->forward('captcha_get');
+    $c->stash(dest => $c->req->params->{'dest'} || $c->req->uri);
+    if ($c->session->{human}) {
+        $c->res->redirect($c->stash->{dest});
     }
     else {
-        $c->session->{human} = 1;
-        $c->res->redirect($c->req->params->{'dest'})
+        $c->forward('captcha_check');
+        if ($c->stash->{recaptcha_error}) {
+            $c->stash( template => 'auth.tt');
+            $c->forward('captcha_get');
+        }
+        else {
+            warn "setting session to 1";
+            $c->session->{human} = 1;
+            $c->res->redirect($c->stash->{dest})
+        }
     }
 }
 
--- Survey-Perl/lib/Survey/Perl/Controller/Survey.pm
+++ Survey-Perl/lib/Survey/Perl/Controller/Survey.pm
@@ -7,6 +7,7 @@ sub survey_base :Chained("/") :PathPart("survey") :CaptureArgs(0) {
     $DB::single=1;
     if (! $c->user) {
         if ($c->req->params->{'dest'} && ! $c->session->{human}) {
+            
             $c->forward('/recaptcha/test_user');
         }
         elsif (! $c->session->{human}) {
@@ -17,7 +18,7 @@ sub survey_base :Chained("/") :PathPart("survey") :CaptureArgs(0) {
 
 sub get_root :Chained("survey_base") :PathPart("") :Args(0) {
     my ($self, $c) = @_;
-    $c->res->body('boo');
+    $c->stash(template => 'survey/index.tt');
 }
 
 
