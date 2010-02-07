e2eaf6ccc1255b11b5d85155fcc71b76697c1cc1
Kieren Diment <kd@fenchurch.local>
2010-02-07 12:20:35 +1100

    update scripts for new catalyst::scriptrunner

--- Survey-Perl/script/survey_perl_test.pl
+++ Survey-Perl/script/survey_perl_test.pl
@@ -1,20 +1,7 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'Survey::Perl';
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-print request($ARGV[0])->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('Survey::Perl', 'Test');
 
 1;
 
@@ -27,7 +14,7 @@ survey_perl_test.pl - Catalyst Test
 survey_perl_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    survey_perl_test.pl http://localhost/some_action
